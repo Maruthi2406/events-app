8c04ea78b0a85877ae67d747a0cbd94b6c3b09a2
Maruthi2406 <pavan.b2406@gmail.com>
2025-07-04 17:41:00 +0530

    Event lister

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import "./App.css";
 
+const message = ["apple", "bat", "cat", "Dog", "Eat"];
+
 export default function App() {
+  function next() {
+    alert("next");
+  }
+
+  function previous() {
+    alert("previous");
+  }
+
+  const x = 1;
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-
-        <p className="message">Hello world</p>
+        <div className={`${x === 1 ? "active" : " "}`}>1</div>
+        <div className={`${x === 2 ? "active" : " "}`}>2</div>
+        <div className={`${x === 3 ? "active" : " "}`}>3</div>
+        <div className={`${x === 4 ? "active" : " "}`}>4</div>
+        <div className={`${x === 5 ? "active" : " "}`}>5</div>
       </div>
 
-      <p className="message">Hello world</p>
+      <p className="message">
+        Task Number : {x} = {message[x - 1]}
+      </p>
 
       <div className="buttons">
-        <button className="previous">Previous</button>
-        <button className="next">Next</button>
+        <button
+          className="previous"
+          onClick={() => previous()}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Previous
+        </button>
+
+        <button
+          className="next"
+          onClick={next}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
