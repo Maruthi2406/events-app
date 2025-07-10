0746c8786210877c79d495779fcb294d80a1906f
Maruthi2406 <pavan.b2406@gmail.com>
2025-07-10 17:05:48 +0530

    useState"hook"

--- src/App.js
+++ src/App.js
@@ -1,49 +1,62 @@
+import { useState } from "react";
 import "./App.css";
 
 const message = ["apple", "bat", "cat", "Dog", "Eat"];
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState(true);
+
   function next() {
-    alert("next");
+    if (count < 5) setCount(count + 1);
   }
 
   function previous() {
-    alert("previous");
+    if (count > 1) setCount(count - 1);
   }
 
-  const x = 1;
+  function close() {
+    setText(!text);
+  }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${x === 1 ? "active" : " "}`}>1</div>
-        <div className={`${x === 2 ? "active" : " "}`}>2</div>
-        <div className={`${x === 3 ? "active" : " "}`}>3</div>
-        <div className={`${x === 4 ? "active" : " "}`}>4</div>
-        <div className={`${x === 5 ? "active" : " "}`}>5</div>
+    <>
+      <div className="close" onClick={close}>
+        x
       </div>
+      {text && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${count === 1 ? "active" : " "}`}>1</div>
+            <div className={`${count === 2 ? "active" : " "}`}>2</div>
+            <div className={`${count === 3 ? "active" : " "}`}>3</div>
+            <div className={`${count === 4 ? "active" : " "}`}>4</div>
+            <div className={`${count === 5 ? "active" : " "}`}>5</div>
+          </div>
 
-      <p className="message">
-        Task Number : {x} = {message[x - 1]}
-      </p>
+          <p className="message">
+            Task Number : {count} = {message[count - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          className="previous"
-          onClick={() => previous()}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          Previous
-        </button>
+          <div className="buttons">
+            <button
+              className="previous"
+              onClick={() => previous()}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              Previous
+            </button>
 
-        <button
-          className="next"
-          onClick={next}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+            <button
+              className="next"
+              onClick={next}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
